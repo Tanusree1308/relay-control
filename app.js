42842bb5e3eabe93b7eeed7802e359fa2776c725
Tanusree1308 <24pa1a0565@vishnu.edu.in>
2025-04-15 16:59:06 +0530

    changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import SerialPort from 'serialport';
+import { SerialPort } from 'serialport'; // ✅ FIXED
 
 dotenv.config();
 
